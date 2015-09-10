f11dac9a09b169c52b516a232f07bd5a7b3c88db
Reed MacConnell <Learn22Ruby@gmail.com>
2015-09-10 11:11:24 -0400

    created dice that can be changed if desired to a specif number between 1 and 6, but no other number otherwise it continues to show the original role.

--- dice.rb
+++ dice.rb
@@ -1,44 +1,22 @@
-=begin
-class Die
-
-  def roll
-    1 + rand(6)
-  end
-
-end
-####################################
-# Let's make a couple of dice...
-dice = [Die.new, Die.new]
-
-# ...and roll them.
-dice.each do |die|
-  puts die.roll
-end
-=end
+#Learn to Program/Chris Pine/Odin Project
+#Chapter 9 Tasks: Dice/instance variables
 
 class Die
   def initialize
     roll
   end
-  def set
-    puts "Set the die."
-    puts "Choose a number 1-6"
-    @chosen = gets.chomp
-    if @chosen =~ /\d/
-      @chosen = @chosen.to_i
-      #Need to fix restricting numbers to between 1 and 6
-      if @chosen > 0 and @chosen < 7
-        @numberShowing = @chosen
-      end
-    else
-      puts "Try again numb nuts."
-      puts Die.new.set
-    end
 
+  def set
+    puts "Change the die, pick a number from 1 - 6."
+    choose = gets.chomp
+    choose = choose.to_i
+    @numberShowing = choose if(choose > 0 and choose < 7)
   end
+
   def roll
     @numberShowing = 1 + rand(6)
   end
+  
   def showing
     @numberShowing
   end
@@ -47,3 +25,4 @@ end
 die = Die.new
 puts "Showing: #{die.showing}"
 puts "Set: #{die.set}"
+puts "Showing again: #{die.showing}"
