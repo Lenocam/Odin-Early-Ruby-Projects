df2d3c7944517c78ef6afcd999f49bb477887181
Reed MacConnell <Learn22Ruby@gmail.com>
2015-09-10 14:54:09 -0400

    added a bunch of methods to orange_tree.rb

--- orange_tree.rb
+++ orange_tree.rb
@@ -5,28 +5,48 @@ class OrangeTree
   #does not immediately produce fruit but eventually does
   def initialize name
     @name = name #that's right your orange tree has a name
-    @height = 1 #height is in feet
+    @height = 4 #height is in 100
     @age = 1
+    @orangeCount = 0
 
-
+    puts @name + ' the sapling is planted'
+    puts "Tree Stats"
+    puts "Orange Count: " + @orangeCount.to_s
+    puts "Tree Height: " + @height.to_s
+    puts "Tree Age: " + @age.to_s
+    puts " "
   end
-  def height #returns height
 
-  end
-
-  def age
-
-
+  def height
+    puts @name + " the tree is " + @height.to_s + " fictional units tall."
   end
 
   def oneYearPasses #ages tree one year
-    @height = @height += 1 #tree grows taller
-    @age = @age += 1
+    if @height <= 15
+      @height += 1 #tree grows taller
+    end
+    @age    += 1
+    countTheOranges
+
+    puts "A year has passed"
+    puts " "
+    puts "Orange Count: " + @orangeCount.to_s
+    puts "Tree Height: " + @height.to_s
+    puts "Tree Age: " + @age.to_s
+    puts " "
     #after multiple years, tree dies
     #unpicked oranges must fall of the tree
+
   end
 
   def countTheOranges #counts the oranges produced
+    if (@age > 1 and @age < 10)
+      @orangeCount += (@age * 2)
+    end
+    if @age > 11
+      @orangeCount -= 2
+    end
+
     #returns the number of oranges on the tree
     #increases @orangeCount each year, maybe peaks then drops
   end
@@ -35,6 +55,50 @@ class OrangeTree
     @orangeCount = @orangeCount -= 1 #reduces @orangeCount by 1
     puts "That is one tasty orange. :)"#sting telling the delicousness of the oranges
     #or no more oranges to pick this year
-
+    puts "Now you have " + @orangeCount.to_s + " oranges left on your tree."
   end
 end
+
+tree = OrangeTree.new 'Frank'
+tree.oneYearPasses
+tree.oneYearPasses
+tree.pickAnOrange
+tree.pickAnOrange
+tree.oneYearPasses
+tree.pickAnOrange
+tree.pickAnOrange
+tree.oneYearPasses
+tree.pickAnOrange
+tree.pickAnOrange
+tree.oneYearPasses
+tree.pickAnOrange
+tree.pickAnOrange
+tree.oneYearPasses
+tree.oneYearPasses
+tree.pickAnOrange
+tree.pickAnOrange
+tree.oneYearPasses
+tree.oneYearPasses
+tree.pickAnOrange
+tree.oneYearPasses
+tree.pickAnOrange
+tree.oneYearPasses
+tree.oneYearPasses
+tree.pickAnOrange
+tree.pickAnOrange
+tree.oneYearPasses
+tree.oneYearPasses
+tree.pickAnOrange
+tree.oneYearPasses
+tree.pickAnOrange
+tree.oneYearPasses
+tree.pickAnOrange
+tree.oneYearPasses
+tree.oneYearPasses
+tree.pickAnOrange
+tree.pickAnOrange
+tree.oneYearPasses
+tree.oneYearPasses
+tree.pickAnOrange
+tree.oneYearPasses
+tree.height
