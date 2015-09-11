e243f52c1a5794a7fce5ec49005388a6377de277
Reed MacConnell <Learn22Ruby@gmail.com>
2015-09-11 15:08:39 -0400

    per instructions I used global variables to add nesting depth to the output from multiple block calls

--- log.rb
+++ log.rb
@@ -1,13 +1,19 @@
 #Learn to Program/Chris Pine/Odin Project
 #Chapter 10 Tasks/Blocks and Procs
 
+$nestingDepth = 0
+$tab = '..'
+
 def log descriptionOfBlock, &someBlock
-  puts "We have started the "+descriptionOfBlock+" block, rejoice"
+  puts $tab*$nestingDepth+"We have started the "+descriptionOfBlock+" block, rejoice"
   puts " "
+  $nestingDepth = $nestingDepth + 1
   blocksReturn = someBlock.call
-  puts "we have finished the "+descriptionOfBlock+" block"
-  puts "Block Returns: "+blocksReturn.to_s
+  $nestingDepth = $nestingDepth -1
+  puts $tab*$nestingDepth+"we have finished the "+descriptionOfBlock+" block"
+  puts $tab*$nestingDepth+"Block Returns: "+blocksReturn.to_s
   puts " "
+
 end
 
 log "outermost" do
@@ -26,5 +32,12 @@ log "outermost" do
 
   log "inner 2" do
     'Sequal not as good as the original?'
+    log "innerest most" do
+      "Testing Testing 1, 2, 3"
+      log "JK, here is a even more interior" do
+        "Depth"
+      end
+    end
   end
+  "done"
 end
