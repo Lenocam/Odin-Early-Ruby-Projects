e75a8223cde1b68cb48a6154c6c28015322938b4
Reed MacConnell <Learn22Ruby@gmail.com>
2015-09-14 15:35:35 -0400

    more practice rubymonk

--- monkruby/rubymonk.rb
+++ monkruby/rubymonk.rb
@@ -1,15 +1,6 @@
-#RubyMonk/Odin Project
-#name of hash
+#rubymonk/odin project
 
-def artax
-  a = [:punch, 0]
-  b = [:kick, 72]
-  c = [:stops_bullets_with_hands, false]
-  key_value_pairs = [a, b, c]
-
-  # you can do this. you are a champion.
-  # unlike Artax, who gave up in a swamp.
-  Hash[key_value_pairs]
+def introduction(age, gender, *names)
+  "Meet #{names.join(' ')}, who's #{age} and #{gender}"
 end
-p artax
-p artax[:kick]
+puts introduction(28, "Male", "Sidu", "Ponnappa", "Chonira")
