6c8feebed8f233a2c1c3589dd4119763f2c273ed
Reed MacConnell <Learn22Ruby@gmail.com>
2015-09-16 09:52:53 -0400

    working on rubymonk problems

--- monkruby/rubymonk.rb
+++ monkruby/rubymonk.rb
@@ -4,3 +4,18 @@ def introduction(age, gender, *names)
   "Meet #{names.join(' ')}, who's #{age} and #{gender}"
 end
 puts introduction(28, "Male", "Sidu", "Ponnappa", "Chonira")
+
+Increment = lambda {|number| number.next }
+
+puts Increment.call(1)
+puts Increment.call(-1)
+puts Increment.call(1000)
+
+#RubyMonk Problems
+array = ["word", "dance", "applesauce", "china"]
+
+def length_finder(input_array)
+  p input_array.map { |item| item.length }
+end
+
+length_finder(array)
