35882f7d4846fe354efc432e8cb4d4a30257b0e4
Reed MacConnell <Learn22Ruby@gmail.com>
2015-09-16 10:10:08 -0400

    rubymonk more problems solved

--- monkruby/rubymonk.rb
+++ monkruby/rubymonk.rb
@@ -12,6 +12,7 @@ puts Increment.call(-1)
 puts Increment.call(1000)
 
 #RubyMonk Problems
+#length of each word in array
 array = ["word", "dance", "applesauce", "china"]
 
 def length_finder(input_array)
@@ -19,3 +20,14 @@ def length_finder(input_array)
 end
 
 length_finder(array)
+puts " "
+
+#split string, count word frequency case insensitive
+frank = 'Do or do not... there is no try.'
+def find_frequency(sentence, word)
+  # Your code here
+  sentence.downcase!
+  sentence.split.count(word)
+end
+
+p find_frequency(frank, "do")
